e3491491c340764d2b9b66417c75587494a16c7e
James Yu <supersecretjames@gmail.com>
2013-04-11 08:44:04 -0700

    Phase V done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :subscriptions_attributes
+  attr_accessible :name, :subscriptions_attributes, :subscription_plan_ids
 
   has_many :subscriptions, :inverse_of => :user
   has_many :subscription_plans, :through => :subscriptions
